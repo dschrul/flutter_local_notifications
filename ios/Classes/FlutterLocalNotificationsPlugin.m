e5fc2c923980141caaba1a1b4645f83b7916c699
dschrul <15520781+dschrul@users.noreply.github.com>
2019-01-22 07:07:11 +0100

    Update FlutterLocalNotificationsPlugin.m

--- ios/Classes/FlutterLocalNotificationsPlugin.m
+++ ios/Classes/FlutterLocalNotificationsPlugin.m
@@ -81,8 +81,6 @@ typedef NS_ENUM(NSInteger, RepeatInterval) {
     headlessRunner = [[FlutterHeadlessDartRunner alloc] init];
     // callbackChannel = [FlutterMethodChannel methodChannelWithName:CALLBACK_CHANNEL binaryMessenger:headlessRunner];
     if(@available(iOS 10.0, *)) {
-        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-        center.delegate = instance;
     }
     [registrar addApplicationDelegate:instance];
     [registrar addMethodCallDelegate:instance channel:channel];
